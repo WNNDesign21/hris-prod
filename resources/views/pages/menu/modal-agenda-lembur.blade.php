254589f3cff3d3ddc618ab02f13e2f2b7ca0a6d1
fathanpr <2010631170072@student.unsika.ac.id>
2024-11-26 17:18:33 +0700

    update notifikasi dan tampilan di menu agenda lembur per user

--- resources/views/pages/menu/modal-agenda-lembur.blade.php
+++ resources/views/pages/menu/modal-agenda-lembur.blade.php
@@ -9,34 +9,77 @@
             <div class="modal-body">
                 <div class="form-group">
                     <div class="row">
-                        <div class="col-12">
-                            <div class="col-12">
-                                <div class="panel-group panel-group-simple panel-group-continuous mb-2"
-                                    id="list-agenda-lembur" aria-multiselectable="true" role="tablist">
-                                    <div class="box">
-                                        <div class="box-body p-0">
-                                            <div class="media-list media-list-hover">
-                                                <div class="media bar-0">
-                                                    <span class="avatar avatar-lg bg-primary-light rounded"><i
-                                                            class="fa fa-user"></i></span>
-                                                    <div class="media-body">
-                                                        <p class="mb-0">
-                                                            <a class="hover-success fs-16" href="#"></a>
-                                                        </p>
-                                                        <h6 class="text-fade my-10">20 September 2024
-                                                        </h6>
-                                                        <h6 class="text-fade my-10">08:00 - 17:00 WIB
-                                                        </h6>
-                                                        <h6>Job Desc : <span class="text-success">2 Subjects</span>
-                                                        </h6>
+                        @if (!empty($agendaLembur))
+                            @foreach ($agendaLembur as $item)
+                                <div class="col-12 px-4">
+                                    <div class="panel-group panel-group-simple panel-group-continuous mb-2"
+                                        id="list-agenda-lembur" aria-multiselectable="true" role="tablist">
+                                        <div class="box">
+                                            <div class="box-header d-flex justify-content-between">
+                                                <h4 class="box-title" style="width: 60%;"><span
+                                                        class="text-success">{{ $item['lembur_id'] }}</span></h4>
+                                                <div>{!! $item['status'] !!}</div>
+                                            </div>
+                                            <div class="box-body p-0">
+                                                <div class="media-list media-list">
+                                                    <div class="media bar-0">
+                                                        <div class="media-body">
+                                                            <p>Deskripsi Pekerjaan : <br>
+                                                                <span
+                                                                    class="text-primary">{{ $item['deskripsi_pekerjaan'] }}</span>
+                                                            </p>
+                                                            <br>
+                                                            <p>Rencana Mulai : <br>
+                                                                <span
+                                                                    class="text-primary">{{ $item['rencana_mulai_lembur'] }}
+                                                                    WIB</span>
+                                                            </p>
+                                                            <br>
+                                                            <p>Rencana Selesai : <br>
+                                                                <span
+                                                                    class="text-primary">{{ $item['rencana_selesai_lembur'] }}
+                                                                    WIB</span>
+                                                            </p>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            @endforeach
+                        @else
+                            <h4 class="text-center p-4">Tidak ada agenda lembur.</h4>
+                        @endif
+
+                        {{-- <div class="col-12 p-4">
+                            <div class="panel-group panel-group-simple panel-group-continuous mb-2"
+                                id="list-agenda-lembur" aria-multiselectable="true" role="tablist">
+                                <div class="box">
+                                    <div class="box-header d-flex justify-content-between">
+                                        <h4 class="box-title"><span class="text-success">Membuat Pak Roni
+                                                Senang</span></h4>
+                                        <span class="badge badge-warning">WAITING</span>
+                                    </div>
+                                    <div class="box-body p-0">
+                                        <div class="media-list media-list">
+                                            <div class="media bar-0">
+                                                <div class="media-body">
+                                                    <p class="mb-0">
+                                                        <a class="hover-success fs-16" href="#"></a>
+                                                    </p>
+                                                    <h6>Rencana Mulai : <span class="text-primary">26 November 2024
+                                                            22:00 WIB</span>
+                                                    </h6>
+                                                    <h6>Rencana Selesai : <span class="text-primary">27
+                                                            November 2024 07:00 WIB</span></h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
