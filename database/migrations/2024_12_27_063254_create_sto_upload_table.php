5f1675d5d3242462c37189abe40d2506653b8c19
Hery Hermawan <sanca.snake@gmail.com>
2024-12-27 15:04:47 +0700

    create new-table for upload to iDem

---
+++ database/migrations/2024_12_27_063254_create_sto_upload_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sto_upload', function (Blueprint $table) {
+            $table->id("id_sto_upload");
+            $table->integer('wh_id');
+            $table->string('wh_name');
+            $table->integer('customer_id');
+            $table->string('customer_name');
+            $table->integer('product_id');
+            $table->string('product_code');
+            $table->string('product_name');
+            $table->string('product_desc');
+            $table->string('model');
+            $table->string('qty_book');
+            $table->string('qty_count');
+            $table->string('processed');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sto_upload');
+    }
+};
