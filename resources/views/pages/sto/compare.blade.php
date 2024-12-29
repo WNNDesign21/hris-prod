22cacdf29085ce535d64bc0768e08f1ddc04fb3d
wandatr19 <wandataufikr@gmail.com>
2024-12-29 08:58:34 +0700

    update order by datatable

--- resources/views/pages/sto/compare.blade.php
+++ resources/views/pages/sto/compare.blade.php
@@ -23,32 +23,21 @@
     <section class="content">
         <div class="row">
             <div class="col-12">
-                {{-- <div class="box"> --}}
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-md-3 col-12">
-                                <div class="form-group">
-                                    <label for="wh_id" class="form-label" >Filter Warehouse</label>
-                                    {{-- <div class="input-group"> --}}
-                                        <select class="form-control" name="wh_id[]" id="wh_id" style="width: 100%;" select2 multiple>
-                                            <option value="">Pilih Warehouse</option>
-                                        </select>
-                                        <button id="submit-filter" type="button" class="btn btn-info">Filter</button>
-                                        <button id="export-excel-button" type="button" class="btn btn-success">Download</button>
-                                    {{-- </div> --}}
-                                    
-                                        
-                                </div>
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-3 col-12">
+                            <div class="form-group">
+                                <label for="wh_id" class="form-label" >Filter Warehouse</label>
+                                    <select class="form-control" name="wh_id[]" id="wh_id" style="width: 100%;" select2 multiple>
+                                        <option value="">Pilih Warehouse</option>
+                                    </select>
+                                    <button id="submit-filter" type="button" class="btn btn-info">Filter</button>
+                                    <button id="export-excel-button" type="button" class="btn btn-success">Download</button>
                             </div>
                         </div>
-                        
-                    {{-- </div> --}}
+                    </div>
                 </div>
                 <div class="box">
-                    {{-- <br>
-                <div class="col-md-2 col-12">
-                    <button id="export-excel-button" class="btn btn-info" type="button">Export to Excel</button>
-                </div> --}}
                     <div class="box-body">
                         <table id="table-hasil-sto" class="table table-bordered table-striped">
                             <thead>
