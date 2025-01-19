67d6dceca247b2f37658bc8731ab72cc48dc264c
fathanpr <2010631170072@student.unsika.ac.id>
2025-01-19 20:37:10 +0700

    update migration change pin column in karyawans

---
+++ database/migrations/2025_01_19_195232_add_pin_new_to_karyawans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('karyawans', function (Blueprint $table) {
+            $table->string('pin_new')->nullable();
+        });
+
+        //gunakan DB statement untuk update pin_new dari pin
+        DB::statement('UPDATE karyawans SET pin_new = pin');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('karyawans', function (Blueprint $table) {
+            $table->dropColumn('pin_new');
+        });
+    }
+};
